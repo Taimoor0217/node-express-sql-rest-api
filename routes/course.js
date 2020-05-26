d45ebaad4990007bb3b0614f5ff45faafc235634
Taimoor Ali <taimoor0217@gmail.com>
2020-05-26 07:27:41 +0500

    course request with id

--- routes/course.js
+++ routes/course.js
@@ -26,6 +26,44 @@ router.get('/', function(req, res){
         })
     })
     .catch(err=>{
+        res.status(400).json({
+            message: "Something went wrong in fetching the course from the database"
+        })
+        console.log(err)
+    })
+});
+
+router.get('/:id', function(req, res){
+    Course
+    .findAll({
+        where: {
+            id: req.params.id
+        },
+        attributes: {
+            exclude: ["createdAt" , "updatedAt" , "userId" , "User.id"],
+        },
+        include:[{
+            model: User,
+            attributes: {
+                exclude: ["password" , "id" , "createdAt" , "updatedAt"]
+            }
+        }]
+    })
+    .then(data=>{
+        if (data.length){
+            res.status(200).json({
+                courses: data
+            })
+        }else{
+            res.status(400).json({
+                message: "The requested course could not be found."
+            })
+        }
+    })
+    .catch(err=>{
+        res.status(400).json({
+            message: "Something went wrong in fetching the course from the database"
+        })
         console.log(err)
     })
 });
